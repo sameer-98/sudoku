0e6c1f42c0c07603c59a9167aaa52880193fc321
Muhammad Sameer Ahmed <91126734+sameer-98@users.noreply.github.com>
2024-05-22 11:10:24 -0600

    fixed logic to display the correct orientation of the board and created function to initialize board

--- game_UI.py
+++ game_UI.py
@@ -1,5 +1,7 @@
 import pygame
-from solver import board
+import random
+from solver import board,board1,board2, solve, print_board
+from copy import deepcopy
 
 class Cube(pygame.sprite.Sprite):
     
@@ -12,7 +14,7 @@ class Cube(pygame.sprite.Sprite):
         self.height = height 
         self.image = pygame.Surface((self.width, self.height))
         self.create_cube()
-        self.rect = self.image.get_rect(topleft=(self.pos[0] * self.width, self.pos[1] * self.height))
+        self.rect = self.image.get_rect(topleft=(self.pos[1] * self.width, self.pos[0] * self.height))
 
 
     def create_cube(self):
@@ -26,19 +28,57 @@ class Cube(pygame.sprite.Sprite):
 
     def update(self, num):
         self.create_cube(num)     
+    
+def get_board():
+
+    boards = [board, board1, board2]
+    current_board = random.choice(boards)
+    solved_board = deepcopy(current_board)
+    if solve(solved_board):
+        return current_board, solved_board
+    return current_board, None
 
 
+def initialize_board(bo, screen):
+    SCREEN_WIDTH, SCREEN_HEIGHT = screen.get_size()
+    cubes = pygame.sprite.Group()
+    for i in range(9):
+        for j in range(9):
+            cube = Cube((i,j), str(bo[i][j]), SCREEN_WIDTH//9, SCREEN_HEIGHT//9)
+            cubes.add(cube)
+    
+    cubes.draw(screen)
+    return cubes
 
-def game_loop(screen, game_active, clock, cubes):
+def game_loop(screen, game_active, clock):
+    bo = []
+    solved_bo = []
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
         
-        if game_active:
-            cubes.draw(screen)
+            if game_active == False and event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bo, solved_bo = get_board()
+                    game_active = True
+                    print_board(bo)
+                    print('**************************')
+                    print_board(solved_bo)
+                    
         
+        if game_active:
+            screen.fill('black')
+            initialize_board(bo, screen)
+            
+            # game_active = solve() == bo 
+
+        else:
+            screen.fill('green')
+        ## Here we will add code to start a new board or reset the board
+
         pygame.display.update()
         clock.tick(60)
 
@@ -46,22 +86,16 @@ def main():
     # Initialize the game
     pygame.init()
 
-    screen = pygame.display.set_mode((540,540))
+    SCREEN_WIDTH = 540
+    SCREEN_HEIGHT = 540
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     pygame.display.set_caption('Sudoku')
     clock = pygame.time.Clock()
-    game_active = True
+    game_active = False
 
-    
-
-    cubes = pygame.sprite.Group()
-    for i in range(9):
-        for j in range(9):
-            cube = Cube((i,j), str(board[i][j]), 540//9, 540//9)
-            cubes.add(cube)
-    
-
-
-    game_loop(screen, game_active, clock, cubes)
+    # solved = solve(board)
+    game_loop(screen, game_active, clock)
 
 if __name__ == '__main__':
     main()
